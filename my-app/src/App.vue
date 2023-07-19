febb7fb23b11af1dd56c3bd1374a8f71513bc54e
Zirus Catalogo <ziruscatalogo@gmail.com>
2023-07-19 12:48:18 +0800

    first commit

---
+++ my-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navigation from './components/Navigation.vue'
+</script>
+
+<template>
+<Navigation/>
+<div>
+hello
+</div>
+</template>
+
+<style >
+</style>
---
+++ my-app/src/components/Navigation.vue
@@ -0,0 +1,11 @@
+<template>
+<nav>
+<div>
+</div> 
+    <ul class="flex justify-center items-center bg-neutral-500 h-16">
+    <li class="mx-4"><a>Home</a></li>
+    <li class="mx-4"><a>Product</a></li>
+    <li class="mx-4"><a>About us</a></li>
+    </ul>
+</nav>
+</template>
